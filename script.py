a2101f434a87111461c0ac7c13c2b83a948fb401
Valentina Staneva <valentina-s@users.noreply.github.com>
2021-01-23 06:07:59 -0800

    Update script.py
    Adding reading file and making spectrogram.

--- script.py
+++ script.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+import obspy
+import requests
+from html.parser import HTMLParser
+import numpy as np
+from datetime import datetime
+
+# extracting link for last file today
+class MyHTMLParser(HTMLParser):
+    def handle_data(self, data):
+        if 'HYVM2' in data:
+            filelist.append(data)
+datestr = datetime.today().strftime('%Y/%m/%d')
+url='https://rawdata.oceanobservatories.org/files/RS01SBPS/PC01A/08-HYDBBA103/{}'.format(datestr)
+r=requests.get(url)
+filelist = []
+parser = MyHTMLParser()
+parser.feed(str(r.content))
+filepath = filelist[-1]
+full_url = f'{url}/{filepath}'
+# reading from url
+st = obspy.read(full_url)
+st.spectrogram(outfile='spectrogram.png')
+
