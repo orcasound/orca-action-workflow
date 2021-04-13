58c845502b67ad02a4d1d4c2f0fe9588b292cfe2
Dmitry Volodin <mr.molkree@gmail.com>
2021-04-13 22:51:03 +0300

    Process last 10 files, not just the last one
    I will try more eventually, but this consumes quite a lot of RAM surprisingly! Not sure who to blame, me or obspy?
    Also just to test multiple items in artifacts

--- script.py
+++ script.py
@@ -1,30 +1,33 @@
 import obspy
 import requests
 from html.parser import HTMLParser
-import numpy as np
-from datetime import datetime
+import datetime
+import re
 
 # extracting link for last file today
 class MyHTMLParser(HTMLParser):
     def handle_data(self, data):
         if 'HYVM2' in data:
             filelist.append(data)
-datestr = datetime.today().strftime('%Y/%m/%d')
-url='https://rawdata.oceanobservatories.org/files/RS01SBPS/PC01A/08-HYDBBA103/{}'.format(datestr)
-r=requests.get(url)
-r=requests.get(url)
-if r == 'Response [404]':
-  datestr = (datetime.today - timedelta(days=1)).strftime('%Y/%m/%d')
-  url='https://rawdata.oceanobservatories.org/files/RS01SBPS/PC01A/08-HYDBBA103/{}'.format(datestr)
-  r=requests.get(url)
+
+yesterday = datetime.datetime.today() - datetime.timedelta(days=2)
+datestr = yesterday.strftime('%Y/%m/%d')
+node = 'PC01A'
+url = f'https://rawdata.oceanobservatories.org/files/RS01SBPS/{node}/08-HYDBBA103/{datestr}'
+
+page = requests.get(url)
+if page.status_code == 404:
+    print('Following directory doesn\'t exist:\n' + url)
+
 filelist = []
 parser = MyHTMLParser()
-parser.feed(str(r.content))
-filepath = filelist[-1]
-full_url = f'{url}/{filepath}'
-# reading from url
-st = obspy.read(full_url)
-st.filter('bandpass', freqmin=2000, freqmax=6000.0)
-st.decimate(factor=10)
-st.spectrogram(outfile='spectrogram.png', dbscale=True, wlen = 0.1)
+parser.feed(str(page.content))
 
+for filename in filelist[-10:]:
+    recording_time = re.search(r'OO-HYVM2--YDH-(.*?)\.mseed', filename).group(1)
+    full_url = f'{url}/{filename}'
+    st = obspy.read(full_url)
+    st.filter('bandpass', freqmin=2000, freqmax=6000.0)
+    st.decimate(factor=10)
+    st.spectrogram(outfile=f'{recording_time}_spectrogram.png', dbscale=True, wlen = 0.1)
+    print('Finished ' + filename)
