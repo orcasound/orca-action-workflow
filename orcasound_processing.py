1960a3f41e182f1ae6eb48a211d5d8a8dca761b3
Dmitry Volodin <mr.molkree@gmail.com>
2021-07-26 19:36:54 +0300

    Small fixes (#39)
    * Fix redefinition of builtin function
    
    * Fix multi-line docstring quotes

--- orcasound_processing.py
+++ orcasound_processing.py
@@ -15,9 +15,9 @@ from create_spectrogram import save_spectrogram
 def convert_with_ffmpeg(input_file, output_file):
     """Converts input file using ffmpeg."""
     try:
-        input = ffmpeg.input(input_file)
-        output = ffmpeg.output(input, output_file)
-        output.run(capture_stdout=True, capture_stderr=True)
+        ffmpeg_input = ffmpeg.input(input_file)
+        ffmpeg_output = ffmpeg.output(ffmpeg_input, output_file)
+        ffmpeg_output.run(capture_stdout=True, capture_stderr=True)
     except ffmpeg.Error as e:
         logging.error(e.stdout.decode("utf8"))
         logging.error(e.stderr.decode("utf8"))
@@ -25,9 +25,11 @@ def convert_with_ffmpeg(input_file, output_file):
 
 
 def create_readable_name(directory, timestamp):
-    """Creates human readable `.wav` file name from `output_dir` and Unix timestamp.
+    """
+    Creates human readable `.wav` file name from `output_dir` and Unix timestamp.
 
-    Resulting name will look like `directory`/%Y-%m-%dT%H-%M-%S.wav"""
+    Resulting name will look like `directory/%Y-%m-%dT%H-%M-%S.wav`
+    """
     return path.join(
         directory,
         f"{datetime.utcfromtimestamp(timestamp).strftime('%Y-%m-%dT%H-%M-%S-%f')[:-3]}.wav",
