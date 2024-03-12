d2217de5645e4bffdf0ef41f50cc73bf1eded620
valentina <vms16@uw.edu>
2024-03-11 20:20:27 -0700

    adding missing lines of code

--- noise_processing.py
+++ noise_processing.py
@@ -3,6 +3,10 @@ from orcasound_noise.pipeline.pipeline import NoiseAnalysisPipeline
 from orcasound_noise.utils import Hydrophone
 import datetime as dt
 
+import pandas as pd
+import matplotlib.pyplot as plt
+from orcasound_noise.pipeline.acoustic_util import plot_spec, plot_bb
+
 
 #Example 1: Port Townsend, 1 Hz Frequency, 60-second samples
 if __name__ == '__main__':
@@ -13,6 +17,10 @@ if __name__ == '__main__':
 psd_path, broadband_path = pipeline.generate_parquet_file(dt.datetime(2023, 3, 23, 7), 
                                                           dt.datetime(2023, 3, 23, 8), 
                                                           upload_to_s3=False)
+
+
+psd_df = pd.read_parquet(psd_path)
+bb_df = pd.read_parquet(broadband_path)
 
 plot_spec(psd_df)
 plot_bb(bb_df)
